e31502683ba6276183fbf6247963c0a46a286a34
MineGame01 <90192686+MineGame01@users.noreply.github.com>
2026-05-18 17:46:07 +0000

    feat: instant search for tasks when typing in the search field

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -4,41 +4,45 @@ import { InputSearchTask } from "./InputSearchTask";
 import { ButtonClearInput } from "./ButtonClearInput";
 
 interface ISearchFormProps {
-    onSubmit: <T extends HTMLElement>(event: React.FormEvent<T>, value: string) => void,
-    setIsSearchTaskMode: (value: boolean) => void,
-    isSearchTaskMode: boolean,
+  onSubmit: <T extends HTMLElement>(
+    event: React.FormEvent<T>,
+    value: string,
+  ) => void;
+  setIsSearchTaskMode: (value: boolean) => void;
+  isSearchTaskMode: boolean;
 }
 
 export const SearchForm: React.FC<ISearchFormProps> = ({
-    setIsSearchTaskMode,
-    onSubmit,
-    isSearchTaskMode
-}) => {    
-    const [searchValue, setSearchValue] = useState("");
-    const inputSearchRef = useRef<HTMLInputElement | null>(null);
+  setIsSearchTaskMode,
+  onSubmit,
+  isSearchTaskMode,
+}) => {
+  const [searchValue, setSearchValue] = useState("");
+  const inputSearchRef = useRef<HTMLInputElement | null>(null);
 
-    const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-        setSearchValue(e.target.value);
-    }
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setSearchValue(e.target.value);
+    onSubmit(e, e.target.value);
+  };
 
-    return <div className="p-1 pre-bg">
-        <form 
-        className="position-relative d-flex"
-        onSubmit={(e) => {
-            onSubmit(e, searchValue);
-        }}>
-            <ButtonSeachTask isSearchTaskMode={isSearchTaskMode} />
-            <InputSearchTask 
-            ref={inputSearchRef}  
-            searchValue={searchValue}
-            onChangeSearch={handleChange}
-            isSearchTaskMode={isSearchTaskMode}
-            setSearchMode={setIsSearchTaskMode}/>
-            {isSearchTaskMode && searchValue.length !== 0 ? 
-                <ButtonClearInput 
-                setSearchValue={setSearchValue} 
-                inputSearchRef={inputSearchRef} /> : null 
-            }
-        </form>
+  return (
+    <div className="p-1 pre-bg">
+      <form className="position-relative d-flex">
+        <ButtonSeachTask isSearchTaskMode={isSearchTaskMode} />
+        <InputSearchTask
+          ref={inputSearchRef}
+          searchValue={searchValue}
+          onChangeSearch={handleChange}
+          isSearchTaskMode={isSearchTaskMode}
+          setSearchMode={setIsSearchTaskMode}
+        />
+        {isSearchTaskMode && searchValue.length !== 0 ? (
+          <ButtonClearInput
+            setSearchValue={setSearchValue}
+            inputSearchRef={inputSearchRef}
+          />
+        ) : null}
+      </form>
     </div>
-}
+  );
+};
